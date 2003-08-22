10ed547a85faf3b885c24b909edeff3605c4f032
gerkey <gerkey@666a4abd-9b63-40ca-8405-2416541b937c>
2003-08-22 20:50:43 +0000

    This module augments laser scan data with the global pose of the laser, assuming that the laser is mounted on a pan-tilt unit (ptu) and that the ptu is mounted on a robot.  At the moment, it is set up to consume segway pose messages and amtec status messages.  The physical mounting of the laser on the ptu and of the ptu on the robot should be reflected in configuration file parameters (look at panlaser.ini).

---
+++ carmen-addons/panlaser/panlaser_main.h
@@ -0,0 +1,36 @@
+/*********************************************************
+ *
+ * This source code is part of the Carnegie Mellon Robot
+ * Navigation Toolkit (CARMEN)
+ *
+ * CARMEN Copyright (c) 2002 Michael Montemerlo, Nicholas
+ * Roy, and Sebastian Thrun
+ *
+ * This module (panlaser) Copyright (c) 2003 Brian Gerkey
+ *
+ * CARMEN is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation; 
+ * either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * CARMEN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General 
+ * Public License along with CARMEN; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, 
+ * Suite 330, Boston, MA  02111-1307 USA
+ *
+ ********************************************************/
+
+#ifndef CARMEN_PANLASER_MAIN_H
+#define CARMEN_PANLASER_MAIN_H
+
+void carmen_panlaser_start(int argc, char** argv);
+void carmen_panlaser_run(void);
+
+#endif
