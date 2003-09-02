5c9fcb78e673931f5094b0b377ba2a3ff1ec5920
mmde <mmde@666a4abd-9b63-40ca-8405-2416541b937c>
2003-09-02 05:33:42 +0000

    *** empty log message ***

--- carmen-addons/segway/segway_messages.h
+++ carmen-addons/segway/segway_messages.h
@@ -36,14 +36,13 @@ typedef struct {
   double pitch, pitch_rate;
   double roll, roll_rate;
   double lw_velocity, rw_velocity;
-  double tv, rv;
   double x, y, theta;
   double timestamp;
   char host[10];
 } carmen_segway_pose_message;
 
 #define CARMEN_SEGWAY_POSE_NAME "carmen_segway_pose"
-#define CARMEN_SEGWAY_POSE_FMT "{double,double,double,double,double,double,double,double,double,double,double,double,[char:10]}"
+#define CARMEN_SEGWAY_POSE_FMT "{double,double,double,double,double,double,double,double,double,double,[char:10]}"
 
 #ifdef __cplusplus
 }
