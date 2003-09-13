f318b24605641c8f4543f2965bc3ea78e13073c5
jrod <jrod@666a4abd-9b63-40ca-8405-2416541b937c>
2003-09-13 01:22:20 +0000

    *** empty log message ***

--- carmen-addons/robotwalker/walker_interface.c
+++ carmen-addons/robotwalker/walker_interface.c
@@ -1,5 +1,6 @@
 #include <carmen/carmen.h>
 #include "walker_interface.h"
+#include "cerebellum_com.h"
 
 
 void carmen_walker_set_goal(int goal) {
@@ -14,4 +15,14 @@ void carmen_walker_set_goal(int goal) {
 
   err = IPC_publishData(CARMEN_WALKER_SET_GOAL_MSG_NAME, &msg);
   carmen_test_ipc(err, "Could not publish", CARMEN_WALKER_SET_GOAL_MSG_NAME);
+}
+
+void carmen_walker_engage_clutch() {
+
+  carmen_cerebellum_engage_clutch();
+}
+
+void carmen_walker_disengage_clutch() {
+
+  carmen_cerebellum_disengage_clutch();
 }
