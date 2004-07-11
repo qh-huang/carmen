b0d7765e7f66cb18a5f2f89ba25cb29ae5c3c6c6
jrod <jrod@666a4abd-9b63-40ca-8405-2416541b937c>
2004-07-11 16:40:50 +0000

    added subscribe_person/trash/door_all to interface

--- carmen-addons/dot/dot_interface.c
+++ carmen-addons/dot/dot_interface.c
@@ -10,6 +10,13 @@ static carmen_handler_t trash_msg_handler_ext = NULL;
 static carmen_dot_door_msg *door_msg_ptr_ext = NULL;
 static carmen_handler_t door_msg_handler_ext = NULL;
 
+static carmen_dot_all_people_msg *all_people_msg_ptr_ext = NULL;
+static carmen_handler_t all_people_msg_handler_ext = NULL;
+static carmen_dot_all_trash_msg *all_trash_msg_ptr_ext = NULL;
+static carmen_handler_t all_trash_msg_handler_ext = NULL;
+static carmen_dot_all_doors_msg *all_doors_msg_ptr_ext = NULL;
+static carmen_handler_t all_doors_msg_handler_ext = NULL;
+
 
 void carmen_dot_reset() {
 
@@ -139,6 +146,174 @@ int carmen_dot_get_all_doors(carmen_dot_door_p *doors, int *num_doors) {
     *num_doors = response->num_doors;
 
   return 0;
+}
+
+static void all_people_msg_handler(MSG_INSTANCE msgRef, BYTE_ARRAY callData,
+			       void *clientData __attribute__ ((unused))) {
+  
+  FORMATTER_PTR formatter;
+  IPC_RETURN_TYPE err = IPC_OK;
+
+  formatter = IPC_msgInstanceFormatter(msgRef);
+
+  if (all_people_msg_ptr_ext)
+    err = IPC_unmarshallData(formatter, callData, all_people_msg_ptr_ext,
+                             sizeof(carmen_dot_all_people_msg));
+  IPC_freeByteArray(callData);
+  carmen_test_ipc_return(err, "Could not unmarshall",
+			 IPC_msgInstanceName(msgRef));
+
+  if (all_people_msg_handler_ext)
+    all_people_msg_handler_ext(all_people_msg_ptr_ext);
+}
+
+void carmen_dot_subscribe_all_people_message(carmen_dot_all_people_msg *msg,
+					 carmen_handler_t handler,
+					 carmen_subscribe_t subscribe_how) {
+
+  IPC_RETURN_TYPE err = IPC_OK;  
+
+  err = IPC_defineMsg(CARMEN_DOT_ALL_PEOPLE_MSG_NAME, IPC_VARIABLE_LENGTH, 
+		      CARMEN_DOT_ALL_PEOPLE_MSG_FMT);
+  carmen_test_ipc_exit(err, "Could not define message", 
+		       CARMEN_DOT_ALL_PEOPLE_MSG_NAME);
+
+  if(subscribe_how == CARMEN_UNSUBSCRIBE) {
+    IPC_unsubscribe(CARMEN_DOT_ALL_PEOPLE_MSG_NAME, 
+		    all_people_msg_handler);
+    return;
+  }
+
+  if (msg)
+    all_people_msg_ptr_ext = msg;
+  else {
+    all_people_msg_ptr_ext = (carmen_dot_all_people_msg *)
+      calloc(1, sizeof(carmen_dot_all_people_msg));
+    carmen_test_alloc(all_people_msg_ptr_ext);
+  }
+
+  all_people_msg_handler_ext = handler;
+  err = IPC_subscribe(CARMEN_DOT_ALL_PEOPLE_MSG_NAME, 
+		      all_people_msg_handler, NULL);
+
+  if(subscribe_how == CARMEN_SUBSCRIBE_LATEST)
+    IPC_setMsgQueueLength(CARMEN_DOT_ALL_PEOPLE_MSG_NAME, 1);
+  else
+    IPC_setMsgQueueLength(CARMEN_DOT_ALL_PEOPLE_MSG_NAME, 100);
+  
+  carmen_test_ipc(err, "Could not subscribe", CARMEN_DOT_ALL_PEOPLE_MSG_NAME);
+}
+
+static void all_trash_msg_handler(MSG_INSTANCE msgRef, BYTE_ARRAY callData,
+			       void *clientData __attribute__ ((unused))) {
+  
+  FORMATTER_PTR formatter;
+  IPC_RETURN_TYPE err = IPC_OK;
+
+  formatter = IPC_msgInstanceFormatter(msgRef);
+
+  if (all_trash_msg_ptr_ext)
+    err = IPC_unmarshallData(formatter, callData, all_trash_msg_ptr_ext,
+                             sizeof(carmen_dot_all_trash_msg));
+  IPC_freeByteArray(callData);
+  carmen_test_ipc_return(err, "Could not unmarshall",
+			 IPC_msgInstanceName(msgRef));
+
+  if (all_trash_msg_handler_ext)
+    all_trash_msg_handler_ext(all_trash_msg_ptr_ext);
+}
+
+void carmen_dot_subscribe_all_trash_message(carmen_dot_all_trash_msg *msg,
+					 carmen_handler_t handler,
+					 carmen_subscribe_t subscribe_how) {
+
+  IPC_RETURN_TYPE err = IPC_OK;  
+
+  err = IPC_defineMsg(CARMEN_DOT_ALL_TRASH_MSG_NAME, IPC_VARIABLE_LENGTH, 
+		      CARMEN_DOT_ALL_TRASH_MSG_FMT);
+  carmen_test_ipc_exit(err, "Could not define message", 
+		       CARMEN_DOT_ALL_TRASH_MSG_NAME);
+
+  if(subscribe_how == CARMEN_UNSUBSCRIBE) {
+    IPC_unsubscribe(CARMEN_DOT_ALL_TRASH_MSG_NAME, 
+		    all_trash_msg_handler);
+    return;
+  }
+
+  if (msg)
+    all_trash_msg_ptr_ext = msg;
+  else {
+    all_trash_msg_ptr_ext = (carmen_dot_all_trash_msg *)
+      calloc(1, sizeof(carmen_dot_all_trash_msg));
+    carmen_test_alloc(all_trash_msg_ptr_ext);
+  }
+
+  all_trash_msg_handler_ext = handler;
+  err = IPC_subscribe(CARMEN_DOT_ALL_TRASH_MSG_NAME, 
+		      all_trash_msg_handler, NULL);
+
+  if(subscribe_how == CARMEN_SUBSCRIBE_LATEST)
+    IPC_setMsgQueueLength(CARMEN_DOT_ALL_TRASH_MSG_NAME, 1);
+  else
+    IPC_setMsgQueueLength(CARMEN_DOT_ALL_TRASH_MSG_NAME, 100);
+  
+  carmen_test_ipc(err, "Could not subscribe", CARMEN_DOT_ALL_TRASH_MSG_NAME);
+}
+
+static void all_doors_msg_handler(MSG_INSTANCE msgRef, BYTE_ARRAY callData,
+			       void *clientData __attribute__ ((unused))) {
+  
+  FORMATTER_PTR formatter;
+  IPC_RETURN_TYPE err = IPC_OK;
+
+  formatter = IPC_msgInstanceFormatter(msgRef);
+
+  if (all_doors_msg_ptr_ext)
+    err = IPC_unmarshallData(formatter, callData, all_doors_msg_ptr_ext,
+                             sizeof(carmen_dot_all_doors_msg));
+  IPC_freeByteArray(callData);
+  carmen_test_ipc_return(err, "Could not unmarshall",
+			 IPC_msgInstanceName(msgRef));
+
+  if (all_doors_msg_handler_ext)
+    all_doors_msg_handler_ext(all_doors_msg_ptr_ext);
+}
+
+void carmen_dot_subscribe_all_doors_message(carmen_dot_all_doors_msg *msg,
+					 carmen_handler_t handler,
+					 carmen_subscribe_t subscribe_how) {
+
+  IPC_RETURN_TYPE err = IPC_OK;  
+
+  err = IPC_defineMsg(CARMEN_DOT_ALL_DOORS_MSG_NAME, IPC_VARIABLE_LENGTH, 
+		      CARMEN_DOT_ALL_DOORS_MSG_FMT);
+  carmen_test_ipc_exit(err, "Could not define message", 
+		       CARMEN_DOT_ALL_DOORS_MSG_NAME);
+
+  if(subscribe_how == CARMEN_UNSUBSCRIBE) {
+    IPC_unsubscribe(CARMEN_DOT_ALL_DOORS_MSG_NAME, 
+		    all_doors_msg_handler);
+    return;
+  }
+
+  if (msg)
+    all_doors_msg_ptr_ext = msg;
+  else {
+    all_doors_msg_ptr_ext = (carmen_dot_all_doors_msg *)
+      calloc(1, sizeof(carmen_dot_all_doors_msg));
+    carmen_test_alloc(all_doors_msg_ptr_ext);
+  }
+
+  all_doors_msg_handler_ext = handler;
+  err = IPC_subscribe(CARMEN_DOT_ALL_DOORS_MSG_NAME, 
+		      all_doors_msg_handler, NULL);
+
+  if(subscribe_how == CARMEN_SUBSCRIBE_LATEST)
+    IPC_setMsgQueueLength(CARMEN_DOT_ALL_DOORS_MSG_NAME, 1);
+  else
+    IPC_setMsgQueueLength(CARMEN_DOT_ALL_DOORS_MSG_NAME, 100);
+  
+  carmen_test_ipc(err, "Could not subscribe", CARMEN_DOT_ALL_DOORS_MSG_NAME);
 }
 
 static void person_msg_handler(MSG_INSTANCE msgRef, BYTE_ARRAY callData,
--- carmen-addons/dot/dot_interface.h
+++ carmen-addons/dot/dot_interface.h
@@ -12,6 +12,17 @@ void carmen_dot_reset();
 int carmen_dot_get_all_people(carmen_dot_person_p *people, int *num_people);
 int carmen_dot_get_all_trash(carmen_dot_trash_p *trash, int *num_trash);
 int carmen_dot_get_all_doors(carmen_dot_door_p *doors, int *num_doors);
+
+void carmen_dot_subscribe_all_people_message(carmen_dot_all_people_msg *msg,
+					     carmen_handler_t handler,
+					     carmen_subscribe_t subscribe_how);
+void carmen_dot_subscribe_all_trash_message(carmen_dot_all_trash_msg *msg,
+					    carmen_handler_t handler,
+					    carmen_subscribe_t subscribe_how);
+void carmen_dot_subscribe_all_doors_message(carmen_dot_all_doors_msg *msg,
+					    carmen_handler_t handler,
+					    carmen_subscribe_t subscribe_how);
+
 void carmen_dot_subscribe_person_message(carmen_dot_person_msg *msg,
 					 carmen_handler_t handler,
 					 carmen_subscribe_t subscribe_how);
