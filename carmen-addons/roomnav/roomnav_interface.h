57b67312aeaa41bbffedf96c162e7c8199276d28
cvs <cvs@666a4abd-9b63-40ca-8405-2416541b937c>
2005-05-10 04:38:01 +0000

    *** empty log message ***

--- carmen-addons/roomnav/roomnav_interface.h
+++ carmen-addons/roomnav/roomnav_interface.h
@@ -11,6 +11,8 @@ extern "C" {
 
 int carmen_roomnav_get_room();
 int carmen_roomnav_get_room_from_point(carmen_map_point_t point);
+int carmen_roomnav_get_room_from_world_point(carmen_point_t point);
+
 void carmen_roomnav_subscribe_room_message(carmen_roomnav_room_msg *room_msg,
 					   carmen_handler_t handler,
 					   carmen_subscribe_t subscribe_how);
