7223ab2bc33df5c89a70c8ada473d8cb1ffbf821
stachnis <stachnis@666a4abd-9b63-40ca-8405-2416541b937c>
2005-12-18 21:20:08 +0000

    vasco-tiny uses now the readlog library

--- carmen/src/vasco/vasco-tiny.c
+++ carmen/src/vasco/vasco-tiny.c
@@ -1,29 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-#include <math.h>
-#include <unistd.h>
-#include <termios.h>
-#include <fcntl.h>
-#include <sys/signal.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <time.h>
-#include <sys/ioctl.h>
+/* #include <stdio.h> */
+/* #include <stdlib.h> */
+/* #include <string.h> */
+/* #include <signal.h> */
+/* #include <math.h> */
+/* #include <unistd.h> */
+/* #include <termios.h> */
+/* #include <fcntl.h> */
+/* #include <sys/signal.h> */
+/* #include <sys/types.h> */
+/* #include <sys/time.h> */
+/* #include <time.h> */
+/* #include <sys/ioctl.h> */
 
 #include <carmen/carmen.h>
+#include <carmen/carmen_stdio.h>
 
 #include "vascocore.h"
 
-#define MAX_NUM_LASER_VALUES              401
-#define MAX_NAME_LENGTH                   256
-#define MAX_LINE_LENGTH                 65536
 
 void
 print_usage( void )
 {
-  fprintf( stderr, "usage: vasco-tiny <LOG-FILE>\n" );
+  fprintf( stderr, "usage: vasco-tiny [options] <carmen-log-file>\n" );
+  fprintf( stderr, "  Options:  -f, --flaser   : use old FLASER instead of ROBOTLASER1\n" );
 }
 
 double
@@ -36,177 +35,121 @@ rad2deg(double x)
 int
 main( int argc, char *argv[] )
 { 
-  carmen_laser_laser_message      scan;
   carmen_point_t                  pos, corrpos, odo;
-  char                            line[MAX_LINE_LENGTH];
-  char                            command[MAX_NAME_LENGTH];
-  char                            dummy[MAX_NAME_LENGTH];
-  char                            host[MAX_NAME_LENGTH];
-  char                          * running;
   double                          time;
   int                             i;
-  FILE                          * fp;
+  int                             scancnt;
   carmen_move_t                   pos_move = {0.0, 0.0, 0.0};
   carmen_move_t                   corr_move = {0.0, 0.0, 0.0};
   carmen_point_t                  old_pos = {0.0, 0.0, 0.0};
   carmen_point_t                  old_corrpos = {0.0, 0.0, 0.0};
+  int use_flaser = 0;
   
-  if (argc!=2) {
+  if (argc < 2) {
     print_usage();
     exit(1);
   }
-  
-  scan.range = (float *) malloc( MAX_NUM_LASER_VALUES * sizeof(float) );
-  carmen_test_alloc(scan.range);
-  
-  scan.remission = (float *) malloc( MAX_NUM_LASER_VALUES * sizeof(float) );
-  carmen_test_alloc(scan.remission);
+
+  scancnt=0;
+  for (i=1; i < argc-1; i++) {
+    if (!strcmp(argv[i], "-f")  || !strcmp(argv[i], "--flaser"))
+      use_flaser = 1;
+  }
 
   carmen_ipc_initialize(argc, argv);
+  vascocore_init(argc, argv);
 
-  /*****************************************************************/
-  vascocore_init( argc, argv );
-  /*****************************************************************/
+  /*   file handler */
+  carmen_FILE *logfile = NULL;
 
-  if ((fp=fopen(argv[1],"r"))==0) {
-    fprintf( stderr, "Can't open input file %s !\n", argv[1] );
-    exit(0);
-  }
+  /*   log file index structure */
+  carmen_logfile_index_p logfile_index = NULL;
 
-  while (fgets(line,MAX_LINE_LENGTH,fp) != NULL) {
+  /*  open logfile for reading */
+  logfile = carmen_fopen(argv[argc-1], "r");
+  if(logfile == NULL)
+    carmen_die("Error: could not open file %s for reading.\n", argv[1]);
 
-    if ( (sscanf(line,"%s",command)!=0) &&
-	 (!strcmp( command, "FLASER") ) ) {
-      sscanf(line, "%s %d",
-	     dummy, &scan.num_readings );
-      running = line;
-      strtok( running, " ");
-      strtok( NULL, " ");
-      for (i=0; i<scan.num_readings; i++) {
-	scan.range[i] = (float) (atof( strtok( NULL, " ") ));
-      }
-      pos.x = atof( strtok( NULL, " ") );
-      pos.y = atof( strtok( NULL, " ") );
-      pos.theta = atof( strtok( NULL, " ") );
-      odo.x = atof( strtok( NULL, " ") );
-      odo.y = atof( strtok( NULL, " ") );
-      odo.theta = atof( strtok( NULL, " ") );
-      scan.timestamp = atof( strtok( NULL, " ") );
-      strncpy( host, strtok( NULL, " "), MAX_NAME_LENGTH );
-      time = atof( strtok( NULL, " ") );
+  /* create index structure */
+  logfile_index = carmen_logfile_index_messages(logfile);
+  
+  /*   buffer for reading the lines */
+  const int max_line_length=9999;
+  char line[max_line_length+1];
+  
+  /*   used to read the messages. erase stucture before! */
+  carmen_robot_laser_message l;
+  carmen_erase_structure(&l, sizeof(carmen_robot_laser_message) );
 
-      // GUESS VALUES
-      scan.config.laser_type = 0;
-      scan.config.start_angle = -0.5*M_PI;
-      scan.config.fov = M_PI;
-      scan.config.angular_resolution = scan.config.fov/ ((double) (scan.num_readings-1.0) );
-      scan.config.maximum_range = 81.92;
-      scan.config.accuracy = 0.05;
-      scan.config.remission_mode = 0;
+  /*   iterate over all entries in the logfile */
+  while (!carmen_logfile_eof(logfile_index)) {
 
-      scan.num_remissions=0;
+    int correct_msg = 1;
+    /*     read the line from the file */
+    carmen_logfile_read_next_line(logfile_index, 
+				  logfile,
+				  max_line_length, 
+				  line);
+    
+    /*     what kind of message it this? */
+    if (use_flaser == 0 && strncmp(line, "ROBOTLASER1 ", 12) == 0) {
+      /*  convert the string using the corresponding read function */
+      char* next = carmen_string_to_robot_laser_message(line, &l);
+      time = atof( strtok( next, " ") );
 
-
-      /*****************************************************************/
-      corrpos = vascocore_scan_match( scan, pos );
-      /*****************************************************************/
-      printf( "FLASER %d", scan.num_readings );
-      for (i=0; i<scan.num_readings; i++) {
-	printf( " %.2f", scan.range[i] );
-      }
-      printf( " %.6f %.6f %.6f %.6f %.6f %.6f %.6f %s %.6f\n",	
-	      corrpos.x, corrpos.y, corrpos.theta,
-	      odo.x, odo.y, odo.theta, scan.timestamp, host, time );
-
-      corr_move = carmen_move_between_points( old_corrpos, corrpos );
-      pos_move = carmen_move_between_points( old_pos, pos );
-
-      fprintf( stderr, "* FLASER  (%d beams) ***\n", scan.num_readings);
-      fprintf( stderr, "expected movement  %.6f m %.6f m %.6f degree\n",
-	       corr_move.forward, corr_move.sideward,
-	       rad2deg(corr_move.rotation) );
-      fprintf( stderr, "corrected movement %.6f m %.6f m %.6f degree\n\n",
-	       pos_move.forward, pos_move.sideward,
-	       rad2deg(pos_move.rotation) );
-
-      
-      old_pos     = pos;
-      old_corrpos = corrpos;
-      
-    } else if ( (sscanf(line,"%s",command)!=0) &&
-	 (!strcmp( command, "ROBOTLASER1") ) ) {
-
-      running = line;
-      strtok( running, " ");
-
-      scan.config.laser_type = (int) (atoi( strtok( NULL, " ") ));
-      scan.config.start_angle = (double) (atof( strtok( NULL, " ") ));
-      scan.config.fov = (double) (atof( strtok( NULL, " ") ));
-      scan.config.angular_resolution = (double) (atof( strtok( NULL, " ") ));
-      scan.config.maximum_range = (double) (atof( strtok( NULL, " ") ));
-      scan.config.accuracy = (double) (atof( strtok( NULL, " ") ));
-      scan.config.remission_mode = (int) (atoi( strtok( NULL, " ") ));
-            
-      scan.num_readings = (int) (atoi( strtok( NULL, " ") ));
-      for (i=0; i<scan.num_readings; i++) {
-	scan.range[i] = (float) (atof( strtok( NULL, " ") ));
-      }
-
-      scan.num_remissions = (int) (atoi( strtok( NULL, " ") ));
-      for (i=0; i<scan.num_remissions; i++) {
-	scan.remission[i] = (float) (atof( strtok( NULL, " ") ));
-      }
-      
-
-      pos.x = atof( strtok( NULL, " ") );
-      pos.y = atof( strtok( NULL, " ") );
-      pos.theta = atof( strtok( NULL, " ") );
-      odo.x = atof( strtok( NULL, " ") );
-      odo.y = atof( strtok( NULL, " ") );
-      odo.theta = atof( strtok( NULL, " ") );
-
-      for (i=0; i<5; i++) {
-	strtok( NULL, " ");
-      }
-
-      scan.timestamp = atof( strtok( NULL, " ") );
-      strncpy( host, strtok( NULL, " "), MAX_NAME_LENGTH );
-      time = atof( strtok( NULL, " ") );
-
-      ///      fprintf(stderr, "SM" );
-
-      /*****************************************************************/
-      corrpos = vascocore_scan_match( scan, pos );
-      /*****************************************************************/
-      printf( "FLASER %d", scan.num_readings );
-      for (i=0; i<scan.num_readings; i++) {
-	printf( " %.2f", scan.range[i] );
-      }
-      printf( " %.6f %.6f %.6f %.6f %.6f %.6f %.6f %s %.6f\n",	
-	      corrpos.x, corrpos.y, corrpos.theta,
-	      odo.x, odo.y, odo.theta, scan.timestamp, host, time );
-
-      corr_move = carmen_move_between_points( old_corrpos, corrpos );
-      pos_move = carmen_move_between_points( old_pos, pos );
-
-      fprintf( stderr, "* ROOBOTLASER1 (%d beams, %d remissions) ***\n", scan.num_readings, scan.num_remissions);
-      fprintf( stderr, "expected movement  %.6f m %.6f m %.6f degree\n",
-	       corr_move.forward, corr_move.sideward,
-	       rad2deg(corr_move.rotation) );
-      fprintf( stderr, "corrected movement %.6f m %.6f m %.6f degree\n\n",
-	       pos_move.forward, pos_move.sideward,
-	       rad2deg(pos_move.rotation) );
-      
-      old_pos     = pos;
-      old_corrpos = corrpos;
-      
-    } else {
-      fputs( line, stdout );
     }
-    fflush(stdout);
-  }
-  fclose(fp);
+    else if (use_flaser == 1 && strncmp(line, "FLASER ", 7) == 0) {
+      /*  convert the string using the corresponding read function */
+      char* next = carmen_string_to_robot_laser_message_orig(line, &l);
+      time = atof( strtok( next, " ") );
+    }
+    else {
+      correct_msg = 0;
+      fputs(line, stdout);
+    }
 
+    if (correct_msg) {
+      odo = l.robot_pose;
+      pos = l.laser_pose;
+
+      carmen_laser_laser_message scan;
+      carmen_erase_structure(&scan,  sizeof(carmen_laser_laser_message) );
+      scan.timestamp      = l.timestamp;
+      scan.config         = l.config;
+      scan.num_readings   = l.num_readings;
+      scan.num_remissions = l.num_remissions;
+      scan.range          = l.range;
+      scan.remission      = l.remission;
+      scan.host           = l.host;
+      corrpos = vascocore_scan_match( scan, pos );
+      scancnt++;
+      corr_move = carmen_move_between_points( old_corrpos, corrpos );
+      pos_move = carmen_move_between_points( old_pos, pos );
+
+      fprintf( stderr, "expected movement  %.6f m %.6f m %.6f degree\n",
+	       corr_move.forward, corr_move.sideward,
+	       rad2deg(corr_move.rotation) );
+      fprintf( stderr, "corrected movement %.6f m %.6f m %.6f degree\n\n",
+	       pos_move.forward, pos_move.sideward,
+	       rad2deg(pos_move.rotation) );
+
+      carmen_logwrite_write_robot_laser(&l,1,(carmen_FILE*)stdout, time);
+      fflush(stdout);
+fflush(stdout);      
+      old_pos     = pos;
+      old_corrpos = corrpos;
+    }
+    
+  }
+  
+  /* close the file */
+  carmen_fclose(logfile);
+
+  fprintf(stderr, "\ndone, corrected %d messages!\n", scancnt);
+
+  /* free index structure */
+  carmen_logfile_free_index(&logfile_index);
+  
   return(0);
   
 }
