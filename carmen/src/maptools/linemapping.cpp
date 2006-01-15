e4121473e1273c54ed7b99beb403a016a8b294e0
nickr <nickr@666a4abd-9b63-40ca-8405-2416541b937c>
2006-01-15 22:50:13 +0000

    Fixed dependence on values.h on Mac

--- carmen/src/maptools/linemapping.cpp
+++ carmen/src/maptools/linemapping.cpp
@@ -4,7 +4,13 @@ extern "C" {
 #endif
 
 #include <carmen/carmen.h>
+#ifndef __APPLE__ 
 #include <values.h>
+#else
+#include <float.h>
+#define MAXDOUBLE DBL_MAX
+#endif
+
 #include "linemapping.h"
 
 #ifdef __cplusplus
