1b3e098cacba532175dafb2626e38e3aae56fa64
stachnis <stachnis@666a4abd-9b63-40ca-8405-2416541b937c>
2006-03-28 10:34:17 +0000

    added documentation

--- carmen/src/logger/writelog.h
+++ carmen/src/logger/writelog.h
@@ -49,34 +49,77 @@ void carmen_logwrite_write_robot_name(char *robot_name,
 
 void carmen_logwrite_write_header(carmen_FILE *outfile);
 
+/** Converts the corresponding message into a string in the carmen log format.
+ * @param odometry The message that should be written to the log file.
+ * @param outfile A carmen file pointer.
+ * @param timestamp The relative timestamp (when was the message received). Note that this is not the timestamp within the message!
+ **/
 void carmen_logwrite_write_odometry(carmen_base_odometry_message *odometry, 
 				    carmen_FILE *outfile, double timestamp);  
 
+/** Converts the corresponding message into a string in the carmen log format.
+ * @param laser The message that should be written to the log file.
+ * @param laser_num The number of the laser (1=front, 2=rear, 3=laser3, 4=laser4, ...)
+ * @param outfile A carmen file pointer.
+ * @param timestamp The relative timestamp (when was the message received). Note that this is not the timestamp within the message!
+ **/
 void carmen_logwrite_write_laser_laser(carmen_laser_laser_message *laser,
 				       int laser_num, carmen_FILE *outfile,
-				       double timestamp);
+					double timestamp);
 
+/** Converts the corresponding message into a string in the carmen log format.
+ * @param laser The message that should be written to the log file.
+ * @param laser_num The number of the laser (1=front, 2=rear, 3=laser3, 4=laser4, ...)
+ * @param outfile A carmen file pointer.
+ * @param timestamp The relative timestamp (when was the message received). Note that this is not the timestamp within the message!
+ **/
 void carmen_logwrite_write_robot_laser(carmen_robot_laser_message *laser,
 				       int laser_num, carmen_FILE *outfile,
 				       double timestamp);
 
+/** Converts the given parameter into a string in the carmen log format.
+ **/
 void carmen_logwrite_write_param(char *module, char *variable, char *value, 
 				 double ipc_time, char *hostname, 
 				 carmen_FILE *outfile, double timestamp);
 
+/** Converts the corresponding message into a string in the carmen log format.
+ * @param sync_message The message that should be written to the log file.
+ * @param outfile A carmen file pointer.
+ **/
 void carmen_logwrite_write_sync(carmen_logger_sync_message *sync_message, 
 				carmen_FILE *outfile);
 
+/** Converts the corresponding message into a string in the carmen log format.
+ * @param truepos The message that should be written to the log file.
+ * @param outfile A carmen file pointer.
+ * @param timestamp The relative timestamp (when was the message received). Note that this is not the timestamp within the message!
+ **/
 void carmen_logwrite_write_truepos(carmen_simulator_truepos_message *truepos, 
 				   carmen_FILE *outfile, double timestamp);  
 
+/** Converts the corresponding message into a string in the carmen log format.
+ * @param msg The message that should be written to the log file.
+ * @param outfile A carmen file pointer.
+ * @param timestamp The relative timestamp (when was the message received). Note that this is not the timestamp within the message!
+ **/
 void carmen_logwrite_write_localize(carmen_localize_globalpos_message *msg, 
 				    carmen_FILE *outfile, double timestamp);
 
+/** Converts the corresponding message into a string in the carmen log format.
+ * @param gps_msg The message that should be written to the log file.
+ * @param outfile A carmen file pointer.
+ * @param timestamp The relative timestamp (when was the message received). Note that this is not the timestamp within the message!
+ **/
 void carmen_logger_write_gps_gpgga(carmen_gps_gpgga_message *gps_msg, 
 				   carmen_FILE *outfile, 
 				   double timestamp);
 
+/** Converts the corresponding message into a string in the carmen log format.
+ * @param gps_msg The message that should be written to the log file.
+ * @param outfile A carmen file pointer.
+ * @param timestamp The relative timestamp (when was the message received). Note that this is not the timestamp within the message!
+ **/
 void carmen_logger_write_gps_gprmc(carmen_gps_gprmc_message *gps_msg, 
 				   carmen_FILE *outfile, 
 				   double timestamp);
