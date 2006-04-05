ae95032027bcc4e690114d6d44b38316ec331bc5
nickroy <nickroy@666a4abd-9b63-40ca-8405-2416541b937c>
2006-04-05 20:01:41 +0000

    Changed sampling to ensure samples are no more 2 std dev. from mean.

--- carmen/src/localize/localize_motion.c
+++ carmen/src/localize/localize_motion.c
@@ -71,11 +71,16 @@ double carmen_localize_sample_noisy_downrange(double delta_t,
 					      *model)
 {
   double downrange_mean, downrange_std_dev;
+  double sample;
 
   downrange_mean = delta_t*model->mean_d_d+delta_theta*model->mean_d_t;
   downrange_std_dev =delta_t*model->std_dev_d_d+delta_theta*model->std_dev_d_t;
 
-  return carmen_gaussian_random(downrange_mean, downrange_std_dev);
+  do {
+    sample = carmen_gaussian_random(downrange_mean, downrange_std_dev);
+  } while (fabs(sample - downrange_mean) > 2*downrange_std_dev);
+
+  return sample; 
 }
 
 double carmen_localize_sample_noisy_crossrange(double delta_t, 
@@ -84,21 +89,31 @@ double carmen_localize_sample_noisy_crossrange(double delta_t,
 					       *model)
 {
   double crossrange_mean, crossrange_std_dev;
+  double sample;
 
   crossrange_mean = delta_t*model->mean_c_d+delta_theta*model->mean_c_t;
   crossrange_std_dev = delta_t*model->std_dev_c_d+
     delta_theta*model->std_dev_c_t;
 
-  return carmen_gaussian_random(crossrange_mean, crossrange_std_dev);
+  do {
+    sample = carmen_gaussian_random(crossrange_mean, crossrange_std_dev);
+  } while (fabs(sample - crossrange_mean) > 2*crossrange_std_dev);
+
+  return sample; 
 }
 
 double carmen_localize_sample_noisy_turn(double delta_t, double delta_theta,
 					 carmen_localize_motion_model_t *model)
 {
   double turn_mean, turn_std_dev;
+  double sample;
 
   turn_mean = delta_t*model->mean_t_d+delta_theta*model->mean_t_t;
   turn_std_dev = delta_t*model->std_dev_t_d+delta_theta*model->std_dev_t_t;
 
-  return carmen_gaussian_random(turn_mean, turn_std_dev);
+  do {
+    sample = carmen_gaussian_random(turn_mean, turn_std_dev);
+  } while (fabs(sample - turn_mean) > 2*turn_std_dev);
+
+  return sample;
 }
