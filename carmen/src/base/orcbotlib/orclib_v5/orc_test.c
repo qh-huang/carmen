04fc48192fb0d0296d6663f4017c2c04a08a631e
fdoshi <fdoshi@666a4abd-9b63-40ca-8405-2416541b937c>
2006-05-08 15:11:03 +0000

    updated makefile to include test

--- carmen/src/base/orcbotlib/orclib_v5/orc_test.c
+++ carmen/src/base/orcbotlib/orclib_v5/orc_test.c
@@ -1,31 +1,3 @@
- /*********************************************************
- *
- * This source code is part of the Carnegie Mellon Robot
- * Navigation Toolkit (CARMEN)
- *
- * CARMEN Copyright (c) 2002 Michael Montemerlo, Nicholas
- * Roy, Sebastian Thrun, Dirk Haehnel, Cyrill Stachniss,
- * and Jared Glover
- *
- * CARMEN is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public 
- * License as published by the Free Software Foundation; 
- * either version 2 of the License, or (at your option)
- * any later version.
- *
- * CARMEN is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied 
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE.  See the GNU General Public License for more 
- * details.
- *
- * You should have received a copy of the GNU General 
- * Public License along with CARMEN; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place, 
- * Suite 330, Boston, MA  02111-1307 USA
- *
- ********************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -34,51 +6,34 @@
 #include "orc.h"
 
 int main( int argn, char **argv ){
+
+
   argn = argn;
   argv = argv;
-  int count;
+
   fprintf( stderr,  " started program... " );
   orc_comms_impl_t *impl = orc_rawprovider_create("/dev/ttyUSB0");
   
   orc_t *orc = orc_create( impl  );
   fprintf( stderr, " made orc \n" );
 
-  orc_motor_set( orc, 0, -127 );
-  orc_motor_set( orc, 2, 127 );
-
-  count = orc_quadphase_read( orc, 1 );
-  fprintf( stderr, "encoder count = %d \n", count );
-
-  sleep(1);
-  count = orc_quadphase_read( orc, 1 );
-  fprintf( stderr, "encoder count = %d \n", count );
-  
-  sleep(1);
-  count = orc_quadphase_read( orc, 1 );
-  fprintf( stderr, "encoder count = %d \n", count );
-
-  sleep(1);
-  count = orc_quadphase_read( orc, 1 );
-  fprintf( stderr, "encoder count = %d \n", count );
-
-  orc_motor_set( orc, 0, 0 );
-  orc_motor_set( orc, 2, 0 );
-
   /*
   // motors and encoders
-  //orc_motor_set( orc, 0, 35 );
-  int count = orc_quadphase_read( orc, 1 );
+  orc_motor_set_signed( orc, 2, -45 );
+  int count = orc_quadphase_read( orc, 2 );
   fprintf( stderr, "encoder count = %d \n", count );
 
-  sleep( 5 );
-  int current =  orc_analog_read(orc, 16);
+  sleep( 1 );
+  int current =  orc_analog_read(orc, 18);
   sleep( 1 );
 
-  //orc_motor_set( orc, 0, 0 );
-  count = orc_quadphase_read( orc, 1 );
+  orc_motor_set_signed( orc, 2, 0 );
+  count = orc_quadphase_read( orc, 2 );
   fprintf( stderr, "current count = %d \n", current );
   fprintf( stderr, "encoder count = %d \n", count );
+  */
 
+  /*
   // analog and digital and pwm io
   orc_pwm_set( orc, 3, 0.75 );
   orc_digital_set( orc, 2, 1 );
@@ -97,10 +52,11 @@ int main( int argn, char **argv ){
   fprintf( stderr, "digital 1 = %d \n", val );
   val = orc_analog_read( orc, 0 );
   fprintf( stderr, "analog 0 = %d \n", val ); 
-
-  fprintf( stderr, "done \n" );
   */
 
+
+  fprintf( stderr, "done \n" );
+  
   // clean up
   orc_destroy( orc );
 
