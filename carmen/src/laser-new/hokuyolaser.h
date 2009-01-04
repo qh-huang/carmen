6dd32347a32b7d617049a490f76ec40a9f554d91
abachrac <abachrac@666a4abd-9b63-40ca-8405-2416541b937c>
2009-01-04 22:28:32 +0000

    decided to hard code the requested beam indices for the various lasers... there were some rounding issues, and I don't really see a reason to not request all the beams that are available

--- carmen/src/laser-new/hokuyolaser.h
+++ carmen/src/laser-new/hokuyolaser.h
@@ -7,20 +7,24 @@
 //#define HOKUYO_ALWAYS_IN_SCIP20
 
 #define HOKUYO_BUFSIZE 8192
-//UTM parameters
-#define UTM_ANGULAR_STEP (M_PI/720.0)
+//UTM parameters from the SCIP 2.0 Spec sheet
+#define UTM_ANGULAR_STEP (2*M_PI/1440.0)
 #define UTM_MAX_BEAMS 1081
+#define UTM_DETECTION_RANGE_START 0
+#define UTM_DETECTION_RANGE_END 1080
 #define UTM_MAX_RANGE 30.0
 #define UTM_ACCURACY .0001
-#define UTM_FOV (270.0*M_PI/180.0)
+#define UTM_FOV (270.25*M_PI/180.0)
 #define UTM_START_ANGLE (-UTM_FOV/2.0)
 
 //URG parameters
-#define URG_ANGULAR_STEP (M_PI/512.0)
-#define URG_MAX_BEAMS 768
+#define URG_ANGULAR_STEP (2*M_PI/1024.0)
+#define URG_MAX_BEAMS 769
+#define URG_DETECTION_RANGE_START 44
+#define URG_DETECTION_RANGE_END 725
 #define URG_MAX_RANGE 5.6
 #define URG_ACCURACY .001
-#define URG_FOV (240.0*M_PI/180.0)
+#define URG_FOV (239.77*M_PI/180.0)
 #define URG_START_ANGLE   (-URG_FOV/2.0) //(-120.0*M_PI/180.0)
 
 
